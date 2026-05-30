cca80537e3009cddfd774dcf6c138b30abcfb3ea
m4nj_un <emonandminjun@gmail.com>
2026-05-30 18:35:49 +0900

    Apply admin UI skin to remaining screens

--- app_admin.py
+++ app_admin.py
@@ -9,7 +9,10 @@ _PROJECT_ROOT = Path(__file__).resolve().parent
 app.static_folder = str(_PROJECT_ROOT / "static")
 app.template_folder = str(_PROJECT_ROOT / "templates")
 
-_ADMIN_LAYOUT_ENDPOINTS = {
+_UI_SKIN_ENDPOINTS = {
+    "login_page",
+    "kiosk_page",
+    "student_home_page",
     "student_manage_page",
     "menu_manage_page",
     "lunch_log_page",
@@ -17,7 +20,7 @@ _ADMIN_LAYOUT_ENDPOINTS = {
     "ai_tools_page",
     "trash_page",
 }
-_ADMIN_LAYOUT_STYLESHEET = '<link rel="stylesheet" href="/static/admin_layout_v5.css">'
+_UI_SKIN_STYLESHEET = '<link rel="stylesheet" href="/static/admin_layout_v5.css">'
 
 
 @app.after_request
@@ -25,14 +28,14 @@ def inject_admin_layout_stylesheet(response):
     content_type = response.headers.get("Content-Type", "")
     if not content_type.startswith("text/html"):
         return response
-    if request.endpoint not in _ADMIN_LAYOUT_ENDPOINTS:
+    if request.endpoint not in _UI_SKIN_ENDPOINTS:
         return response
 
     body = response.get_data(as_text=True)
     if "admin_layout_v5.css" in body or "</head>" not in body:
         return response
 
-    response.set_data(body.replace("</head>", f"  {_ADMIN_LAYOUT_STYLESHEET}\n</head>", 1))
+    response.set_data(body.replace("</head>", f"  {_UI_SKIN_STYLESHEET}\n</head>", 1))
     return response
 
 
